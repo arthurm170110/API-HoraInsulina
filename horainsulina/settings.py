195658cab55a7623bc50094fc55b8c376cca00ea
Arthur Lima <61024534+arthurm170110@users.noreply.github.com>
2023-05-01 23:45:19 -0300

    Update settings.py

--- horainsulina/settings.py
+++ horainsulina/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'horainsulina.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'EJOIVmJXiX5gbqvU7nWi',
+        'HOST': 'containers-us-west-103.railway.app',
+        'PORT': '6743',
     }
 }
 
